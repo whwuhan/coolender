735490c60a599a1dc85190d2657e4e2eeffb8aa2
whwuhan <695549978@qq.com>
2021-02-14 22:35:35 +0800

    调整目录结构

--- 3rdsrc/whlib/test/makefile/test_binary_tree.mk
+++
@@ -1,8 +0,0 @@
-include config/*.mk
-
-test_binary_tree : 
-	$(CC) $(TEST_MAIN_DIR)/test_binary_tree.cpp -o $(TEST_DES_DIR)/test_binary_tree
-
-.PHONY : clean
-clean :  
-	-rm $(DES_DIR)test_binary_tree
