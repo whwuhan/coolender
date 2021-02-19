a58de544e0337fe38f45af48da86e09ac7bdb20c
whwuhan <695549978@qq.com>
2021-02-19 11:30:26 +0800

    更改UI字体

--- include/coolender_ui.h
+++ include/coolender_ui.h
@@ -12,13 +12,32 @@ namespace coolender
         CoolenderUI();
         void init(GLFWwindow* window);//初始化，在渲染循环外
         void renderDemoUI();//根据scence，渲染UI
-        void render();
+        void render();//渲染所有的UI
         void terminate();//清空数据
 
-    private:
-        bool showFileChooseDialog;//文件选择对话框
+        static bool showFileChooseDialog;//文件选择对话框
+        static bool showUsage;//显示操作手册
+        static bool showRightSideBar;//显示右边Sidebar
+        
 
+    private:
+        ImGuiIO* io;//ImGui io
+        float fontSize;//字体大小
+        float globalScale;//整体的字体缩放
+        //一级UI
         void renderMenu();//顶部菜单栏
+        void renderUsage();//使用手册
+        void renderRightSideBar();//右边的Side Bar
+        
+
+
+        //二级UI
+
+
+
+
+        //隐藏UI
         void renderFileChooseDialog();//渲染文件选择对话框
+        
     };
 }
--- src/coolender_ui.cpp
+++ src/coolender_ui.cpp
@@ -1,8 +1,15 @@
 #include <coolender_ui.h>
 using namespace coolender;
 using namespace std;
+
+//static 变量初始化
+bool CoolenderUI::showUsage = true;
+bool CoolenderUI::showRightSideBar = true;
+bool CoolenderUI::showFileChooseDialog = false;
+
 CoolenderUI::CoolenderUI():
-showFileChooseDialog(false)
+globalScale(1.0),
+fontSize(15.0)
 {}
 
 //初始化UI
@@ -11,7 +18,11 @@ void CoolenderUI::init(GLFWwindow* window)
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGuiIO& io = ImGui::GetIO(); 
+    (void)io;
+    this->io = &io;
+    //io.FontGlobalScale = globalScale;//设置字体的缩放大小
+    io.Fonts->AddFontFromFileTTF("fonts/DroidSans.ttf", fontSize);//设置字体
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
@@ -39,7 +50,7 @@ void CoolenderUI::renderDemoUI()
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
-//渲染UI
+//渲染所有UI
 void CoolenderUI::render()
 {
         ImGui_ImplOpenGL3_NewFrame();
@@ -53,9 +64,9 @@ void CoolenderUI::render()
 
         // We specify a default position/size in case there's no data in the .ini file.
         // We only do it to make the demo applications a little more welcoming, but typically this isn't required.
-        const ImGuiViewport* main_viewport = ImGui::GetMainViewport();
-        ImGui::SetNextWindowPos(ImVec2(main_viewport->WorkPos.x + 650, main_viewport->WorkPos.y + 20), ImGuiCond_FirstUseEver);
-        ImGui::SetNextWindowSize(ImVec2(100, 68), ImGuiCond_FirstUseEver);
+        // const ImGuiViewport* main_viewport = ImGui::GetMainViewport();
+        // ImGui::SetNextWindowPos(ImVec2(main_viewport->WorkPos.x + 650, main_viewport->WorkPos.y + 20), ImGuiCond_FirstUseEver);
+        // ImGui::SetNextWindowSize(ImVec2(100, 68), ImGuiCond_FirstUseEver);
         // ImGuiWindowFlags windowFlags = 0;
         //     // Demonstrate the various window flags. Typically you would just use the default!
         // static bool no_titlebar = false;
@@ -79,24 +90,29 @@ void CoolenderUI::render()
         // if (no_bring_to_front)  windowFlags |= ImGuiWindowFlags_NoBringToFrontOnFocus;
 
         //初始界面显示部分
-        bool open = true;
 
-        ImGui::Begin("Coolender", &open);
-        ImGui::Text("PROGRAMMER GUIDE:");
-        ImGui::End();
+        //使用手册
+        if(CoolenderUI::showUsage)
+        {
+            renderUsage();
+        }
+        //右侧Sidebar
+        if(CoolenderUI::showRightSideBar)
+        {
+            renderRightSideBar();
+        }
+        
         //初始显示部分结束
 
 
 
         //界面隐藏部分
         //显示文件选择框
-        if(showFileChooseDialog)
+        if(CoolenderUI::showFileChooseDialog)
         {
             renderFileChooseDialog();
         }
         
-
-
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
@@ -104,17 +120,20 @@ void CoolenderUI::render()
 //渲染顶部菜单栏
 void CoolenderUI::renderMenu()
 {
+    //顶部菜单栏
     if (ImGui::BeginMainMenuBar())
-    {
+    {   
+        //File
         if (ImGui::BeginMenu("File"))
         {
             if (ImGui::MenuItem("open", "CTRL+O"))
             {
-                showFileChooseDialog = true;
+                CoolenderUI::showFileChooseDialog = true;
             }
             ImGui::EndMenu();
         }
-
+        
+        //Edit
         if (ImGui::BeginMenu("Edit"))
         {
             if (ImGui::MenuItem("Undo", "CTRL+Z"))
@@ -128,8 +147,58 @@ void CoolenderUI::renderMenu()
             if (ImGui::MenuItem("Paste", "CTRL+V")) {}
             ImGui::EndMenu();
         }
+
+        //Window 控制显示哪些窗口
+        if (ImGui::BeginMenu("Window"))
+        {
+            //使用手册
+            if (ImGui::MenuItem("Usage", NULL, CoolenderUI::showUsage))
+            {
+                CoolenderUI::showUsage = true;
+            }
+
+            //右侧Sidebar
+            if (ImGui::MenuItem("Show Right Sidebar", NULL, CoolenderUI::showRightSideBar))
+            {
+                CoolenderUI::showRightSideBar = true;
+            }
+            ImGui::EndMenu();
+        }
         ImGui::EndMainMenuBar();
     }
+}
+
+//渲染使用手册窗口
+void CoolenderUI::renderUsage()
+{   
+    //设置大小和位置
+    const ImGuiViewport* main_viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(ImVec2(main_viewport->WorkPos.x + 30, main_viewport->WorkPos.y + 50), ImGuiCond_FirstUseEver);
+    //ImGui::SetNextWindowSize(ImVec2(400, 200), ImGuiCond_FirstUseEver);
+    
+    //窗口内容
+    ImGui::Begin("Usage", &CoolenderUI::showUsage, ImGuiWindowFlags_AlwaysAutoResize);
+    {
+        ImGui::Text("Please press Q to change operate mode!");
+        ImGui::Separator();
+        ImGui::Text("In cursor operate mode:");
+        ImGui::Text("You can use cursor to select what you want.");
+        ImGui::Separator();
+        ImGui::Text("In movement operate mode:");
+        ImGui::Text("Press WASD and SPACE key to move your view position.");
+        ImGui::Text("Move your mouse to change view angle.");
+        ImGui::Text("Scroll the mouse wheel to change the field of view.");
+    }
+
+    ImGui::End();
+}
+
+//渲染右侧Sidebar
+void CoolenderUI::renderRightSideBar()
+{
+    ImGui::Begin("Coolender", &CoolenderUI::showRightSideBar);
+    ImGui::Text("PROGRAMMER GUIDE:");
+    ImGui::End();
 }
 
 //渲染文件选择框
@@ -148,10 +217,10 @@ void CoolenderUI::renderFileChooseDialog()
             cout << "You choose file:" << filePathName << endl;
             cout << "Your choosed file’s path is " << filePath << endl;
             ImGuiFileDialog::Instance()->Close();
-            showFileChooseDialog = false;
+            CoolenderUI::showFileChooseDialog = false;
         }
         // close
         ImGuiFileDialog::Instance()->Close();
-        showFileChooseDialog = false;
+        CoolenderUI::showFileChooseDialog = false;
     }        
 }
