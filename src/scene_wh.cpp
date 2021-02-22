ca15e5da71fcbc7a7ed15a013c5d9962214a9d97
whwuhan <695549978@qq.com>
2021-02-22 14:03:08 +0800

    添加wh后缀，防止与assimp中的scene.h scene.cpp重名

---
+++ src/scene_wh.cpp
@@ -0,0 +1,62 @@
+#include <scene_wh.h>
+using namespace std;
+using namespace coolender;
+using namespace wh::basic;
+using namespace glm;
+vec4 Scene::clearColor(1.0f, 1.0f, 1.0f, 1.0f);
+bool Scene::showFloor = true;
+bool Scene::phongLightingModel = true;
+
+
+//注意static变量要先初始化 否则链接失败
+map<string, Light> Scene::lightCollection;//光源
+map<string, PointCloud> Scene::pointCloudCollection;//点云
+map<string, PolygonMesh> Scene::polygonMeshCollection;//Mesh网格
+
+Scene::Scene()
+{}
+
+// void Scence::init()
+// {}
+
+//添加光源
+void Scene::addLight(string lightName, Light &light)
+{
+    lightCollection[lightName] = light;
+    cout << "Add light successfully." << endl;
+}
+
+//添加点云
+void Scene::addPointCloud(string pointCloudName, PointCloud &pointCloud)
+{
+    pointCloudCollection[pointCloudName] = pointCloud;
+    cout << "Add point cloud successfully." << endl;
+}
+
+//添加Mesh
+void Scene::addPolygonMesh(string polygonMeshName, PolygonMesh &polygonMesh)
+{
+    polygonMeshCollection[polygonMeshName] = polygonMesh;
+    cout << "Add polygonmesh successfully." << endl;
+}
+
+//删除光源
+void Scene::deleteLight(string lightName)
+{
+    lightCollection.erase(lightName);
+    cout << "Delete light successfully." << endl;
+}
+
+//删除点云
+void Scene::deletePointCloud(string pointCloudName)
+{
+    pointCloudCollection.erase(pointCloudName);
+    cout << "Delete point cloud successfully." << endl;
+}
+
+//删除Mesh
+void Scene::deletePolygonMesh(string polygonMeshName)
+{
+    polygonMeshCollection.erase(polygonMeshName);
+    cout << "Delete polygonmesh successfully." << endl;
+}
